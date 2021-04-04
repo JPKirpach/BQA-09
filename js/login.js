485d4bb6111fdfd49507316f75032801df643619
Juan Pablo Kirpach <jpkirpach@gmail.com>
2021-04-04 20:08:44 -0300

    BQA-09: Tests completed

--- js/login.js
+++ js/login.js
@@ -6,8 +6,6 @@ function formValidation(){
   return false
 }
 
-console.log(formValidation())
-
 function inputValidation(inputType){
   var inputField = document.querySelector("input[type="+inputType+"]")
   if(inputField != undefined){
@@ -16,9 +14,43 @@ function inputValidation(inputType){
   return false
 }
 
+function buttonValidation(numberOfButtons){
+  var documentButtons = document.querySelectorAll('button');
+  if(documentButtons.length == numberOfButtons){
+    return true;
+  }
+  return false;
+}
+
+function labelValidation(numberOfLabels){
+  var documentLabels = document.querySelectorAll('label');
+  if(documentLabels.length == numberOfLabels){
+    return true;
+  }
+  return false;
+}
+
+function changePageValidation(){
+  var documentLinks = document.querySelectorAll('a');
+  if(documentLinks.length == 1){
+    return true
+  }
+  return false
+}
+
+
+console.log(changePageValidation())
+
+console.log(formValidation())
+
+
 requiredInputs = ['email', 'password']
 for(i=0; i < requiredInputs.length; i++){
   console.log(inputValidation(requiredInputs[i]))
 }
 
 
+console.log(buttonValidation(1))
+
+
+console.log(labelValidation(2))
--- js/register.js
+++ js/register.js
@@ -1,24 +1,59 @@
 function formValidation(){
-  var registerForm = document.querySelector('form')
+  var registerForm = document.querySelector('form');
   if(registerForm != undefined){
-    return true
+    return true;
   }
-  return false
+  return false;
 }
-
-console.log(formValidation())
 
 function inputValidation(inputType){
-  var inputField = document.querySelector("input[type="+inputType+"]")
+  var inputField = document.querySelector("input[type="+inputType+"]");
   if(inputField != undefined){
+    return true;
+  }
+  return false;
+}
+
+function confirmPass(){
+  var confPass = document.querySelectorAll("input[type='password']");
+  if(confPass.length == 2){
+    return true;
+  }
+  return false;
+}
+
+function buttonValidation(numberOfButtons){
+  var documentButtons = document.querySelectorAll('button');
+  if(documentButtons.length == numberOfButtons){
+    return true;
+  }
+  return false;
+}
+
+function labelValidation(numberOfLabels){
+  var documentLabels = document.querySelectorAll('label');
+  if(documentLabels.length == numberOfLabels){
+    return true;
+  }
+  return false;
+}
+
+function changePageValidation(){
+  var documentLinks = document.querySelectorAll('a');
+  if(documentLinks.length == 2){
     return true
   }
-  return false
+  return true
 }
 
-requiredInputs = ['email', 'password', 'text']
+requiredInputs = ['email', 'password', 'text'];
 
 for(i=0; i < requiredInputs.length; i++){
-  console.log(inputValidation(requiredInputs[i]))
+  console.log(inputValidation(requiredInputs[i]));
 }
 
+console.log(buttonValidation(2))
+
+console.log(labelValidation(4))
+
+console.log(changePageValidation())
